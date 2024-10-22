3839e1d6cd7a39d93a1c5957e36a36d60c843c96
Verni <vernidev@gmail.com>
2024-10-22 23:12:35 +0200

    Extract prefix to static variable in notifications.

--- eternaleconomy-core/src/main/java/com/eternalcode/economy/command/cooldown/CommandCooldownConfig.java
+++ eternaleconomy-core/src/main/java/com/eternalcode/economy/command/cooldown/CommandCooldownConfig.java
@@ -1,10 +1,13 @@
 package com.eternalcode.economy.command.cooldown;
 
+import com.eternalcode.economy.config.implementation.messages.MessageConfig;
 import com.eternalcode.multification.notice.Notice;
 import eu.okaeri.configs.OkaeriConfig;
 import eu.okaeri.configs.annotation.Comment;
 
 import java.time.Duration;
+
+import static com.eternalcode.economy.config.implementation.messages.MessageConfig.*;
 
 public class CommandCooldownConfig extends OkaeriConfig {
     @Comment("Duration of the cooldown (e.g. 5s, 10m, 1h)")
@@ -12,8 +15,7 @@ public class CommandCooldownConfig extends OkaeriConfig {
     @Comment("Permission for admins to bypass the cooldown")
     public String bypassPermission = "eternaleconomy.player.pay.bypass";
     public Notice message = Notice.builder()
-        .chat("<b><gradient:#00FFA2:#34AE00>ECONOMY</gradient></b> <dark_gray>➤</dark_gray> "
-            + "<white>You must wait <gradient:#00FFA2:#34AE00>{TIME}</gradient> before using /pay again.")
+        .chat(messagesPrefix + "<white>You must wait <gradient:#00FFA2:#34AE00>{TIME}</gradient> before using /pay again.")
         .actionBar("<gradient:#00FFA2:#34AE00>Wait {TIME}!</gradient>")
         .build();
 }
