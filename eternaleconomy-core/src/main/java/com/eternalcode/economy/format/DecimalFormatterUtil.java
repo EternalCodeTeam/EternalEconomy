4bf56f2b5cb7f82106dfe6f41173e693f0394119
vuxeim <35169070+vuxeim@users.noreply.github.com>
2026-06-28 20:13:55 +0200

    make code faster by embracing math

--- eternaleconomy-core/src/main/java/com/eternalcode/economy/format/DecimalFormatterUtil.java
+++ eternaleconomy-core/src/main/java/com/eternalcode/economy/format/DecimalFormatterUtil.java
@@ -2,11 +2,13 @@ package com.eternalcode.economy.format;
 
 import static java.lang.Math.ceil;
 import static java.lang.Math.floor;
+import static java.lang.Math.log10;
 
 import java.math.BigDecimal;
 
 /**
  * @author Rafał "shitzuu" Chomczyk
+ * @author vuxeim@pm.me <a href="https://vuxe.im/">vuxeim</a>
  **/
 final class DecimalFormatterUtil {
 
@@ -15,8 +17,7 @@ final class DecimalFormatterUtil {
     }
 
     public static long getIntegralPart(final double amount) {
-        final double fractionalPart = getFractionalPart(amount);
-        return (long) (amount - fractionalPart);
+        return (long) amount;
     }
 
     public static long getIntegralPart(final BigDecimal amount) {
@@ -24,7 +25,19 @@ final class DecimalFormatterUtil {
     }
 
     public static int getLengthOfIntegralPart(final double amount) {
-        return Long.toString(getIntegralPart(amount)).length();
+        // unbelievably faster than converting to string and getting length
+        /*
+        in Java:
+            log10(0) == -Infinity
+            log10(negative number) == NaN
+        in real arithmetic:
+            log10(0) is undefined
+            log10(negative number) is undefined
+        */
+        final long integralPart = getIntegralPart(amount);
+        if (integralPart == 0) { return 1; }
+        if (integralPart < 0) { return getLengthOfIntegralPart(-integralPart); } // add +1 if minus sign has to be taken into account
+        return 1 + (int) getIntegralPart(log10(integralPart));
     }
 
     public static int getLengthOfIntegralPart(final BigDecimal amount) {
